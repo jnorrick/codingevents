0ae290f63322a05b5215201f5d0e55baccb49f1a
Jessica Orrick <jessica.orrick@outlook.com>
2024-01-22 14:37:43 -0600

    added event handler for post and updated events\index and templates\index

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,7 +3,9 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,20 +14,25 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
+    private static List<String> events = new ArrayList<>();
+
     @GetMapping
     public String displayAllEvents(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("Code and Coffee");
-        events.add("DevFest");
-        events.add("Completely Real and Not Fake Event");
-        events.add("Code Til Dawn");
         model.addAttribute("events", events);
         return "events/index";
     }
 
     // lives at /events/create
     @GetMapping("create")
-    public String renderCreateEventForm() {
+    public String displayCreateEventForm() {
         return "events/create";
     }
+
+    // lives at /events/create
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName) {
+        events.add(eventName);
+        return "redirect:/events";
+    }
+
 }
